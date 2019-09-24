0cc63566eed5b6ed3e52fbc07dcd495230d48b9a
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-09-24 19:39:50 +0700

    Update index.php
    modal untuk insert record baru rekam medis

--- application/views/admin/rekammedis/detail/index.php
+++ application/views/admin/rekammedis/detail/index.php
@@ -73,7 +73,7 @@
         </form>
 
         <div class="row">
-            <div class="col-md-6" >
+            <div class="col-md-6">
                 <div class="form-group row">
                     <label for="status" class="col-sm-4 col-form-label">Status Hewan</label>
                     <div class="col-sm-8">
@@ -89,7 +89,7 @@
             </div>
             <div class="col-md-6">
                 <div style="margin-left:5cm">
-                    <button class="btn btn-secondary" >Insert New Record</button>
+                    <button class="btn btn-secondary" id="myBtn">Insert New Record</button>
                     <button class="btn btn-secondary">Download Form Record</button>
                     <!--<button class="btn btn-secondary">Delete Record</button>-->
                 </div>
@@ -122,7 +122,72 @@
                     </tbody>
                 </table>
             </div>
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
 
+                    <!-- Modal content-->
+                    <div class="modal-content" style="padding:40px 50px;">
+
+                        <div class="modal-header">
+                            <h4 class="modal-title">New Record</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <form role="form" style="margin-top:0.5cm">
+                            <div class="form-group">
+                                <label for="gejalaKlinis" class="">Gejala Klinis</label>
+                                <input type="text" name="gejalaKlinis" class="form-control" id="" placeholder="gejala klinis">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="suhuBadan" class="">Suhu Badan</label>
+                                <input type="text" name="suhuBadan" class="form-control" id="" placeholder="suhu badan">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="beratBadan" class="">Berat Badan</label>
+                                <input type="text" name="beratBadan" class="form-control" id="" placeholder="berat badan">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="diagnosa" class="">Diagnosa</label>
+                                <input type="text" name="diagnosa" class="form-control" id="" placeholder="diagnosa">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="tindakan" class="">Tindakan</label>
+                                <input type="text" name="tindakan" class="form-control" id="" placeholder="tindakan">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="idDokter" class="">Diperiksa Oleh</label>
+
+                                <div class="input-group mb-3">
+                                    <select name="role" class="custom-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                    </select>
+                                </div>
+
+                            </div>
+                        </form>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary">Submit</button>
+                            <!--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
+
 </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $("#myBtn").click(function() {
+            $("#myModal").modal();
+        });
+    });
+</script>
