aea72c27b4433a7cde9a4e70220ccd6e0340ae43
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-09-25 18:38:27 +0700

    Update v_listPasien.php

--- application/views/admin/datapasien/v_listPasien.php
+++ application/views/admin/datapasien/v_listPasien.php
@@ -16,7 +16,7 @@
                 <th>Nama</th>
                 <th>Alamat</th>
                 <th>No. Telepon</th>
-                <th></th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
