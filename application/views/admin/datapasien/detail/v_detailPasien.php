02bba7992ea2c6f4837b6f81d248b0dfa58783c6
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-09-30 15:19:50 +0700

    Update v_detailPasien.php

--- application/views/admin/datapasien/detail/v_detailPasien.php
+++ application/views/admin/datapasien/detail/v_detailPasien.php
@@ -70,13 +70,13 @@
                             <tbody>
                                 <?php foreach ($data_hewan as $d) { ?>
                                     <tr>
-                                        <td><?php echo $d->idHewan?></td>
-                                        <td><?php echo $d->namaHewan?></td>
+                                        <td><?php echo $d->idHewan ?></td>
+                                        <td><?php echo $d->namaHewan ?></td>
                                         <td></td>
                                         <td><?php echo $d->tanggalLahir ?></td>
                                         <td><?php echo gender($d->jenisKelamin) ?></td>
                                         <td><?php echo $d->ras ?></td>
-                                        <td><?php echo $d->warna?></td>
+                                        <td><?php echo $d->warna ?></td>
                                     </tr>
                                 <?php } ?>
                             </tbody>
@@ -89,15 +89,23 @@
 
                                 <!-- Modal content-->
                                 <div class="modal-content" style="padding:40px 50px;">
-
+                                    <?php if ($this->session->flashdata('success')) : ?>
+                                        <div class="alert alert-success" role="alert">
+                                            <?php echo $this->session->flashdata('success'); ?>
+                                        </div>
+                                    <?php endif; ?>
                                     <div class="modal-header">
-                                        <h4 class="modal-title">Edit Data Hewan</h4>
+                                        <h4 class="modal-title">Tambah Data Hewan</h4>
                                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                                     </div>
-                                    <form role="form" style="margin-top:0.5cm">
+
+                                    <form class="data-pasien" role="form" style="margin-top:0.5cm" action="<?php site_url('admin/formDataHewan'); ?>" method="post" enctype="multipart/form-data">
                                         <div class="form-group">
                                             <label for="namaHewan" class="">Nama Hewan</label>
-                                            <input type="text" name="namaHewan" class="form-control" id="" placeholder="nama hewan">
+                                            <input type="text" name="namaHewan" class="form-control <?php echo form_error('namaHewan') ? 'is-invalid' : '' ?>" id="" placeholder="nama hewan">
+                                            <div class="invalid-feedback">
+                                                <?php echo form_error('namaHewan') ?>
+                                            </div>
                                         </div>
 
                                         <div class="form-group">
@@ -121,30 +129,40 @@
 
                                         <div class="form-group">
                                             <label for="tanggalLahir">Tanggal Lahir</label>
-                                            <input type="date" name="tanggalLahir" class="tanggal form-control" placeholder="tanggal lahir">
+                                            <input type="date" name="tanggalLahir" class="tanggal form-control <?php echo form_error('tanggalLahir') ? 'is-invalid' : '' ?>" placeholder="tanggal lahir">
+                                            <div class="invalid-feedback">
+                                                <?php echo form_error('tanggalLahir') ?>
+                                            </div>
                                         </div>
 
                                         <div class="form-group">
                                             <label for="ras" class="">Ras</label>
-                                            <input type="text" name="ras" class="form-control" id="" placeholder="ras">
+                                            <input type="text" name="ras" class="form-control <?php echo form_error('ras') ? 'is-invalid' : '' ?>" id="" placeholder="ras">
+                                            <div class="invalid-feedback">
+                                                <?php echo form_error('ras') ?>
+                                            </div>
                                         </div>
-
                                         <div class="form-group">
                                             <label for="warna" class="">Warna</label>
-                                            <input type="text" name="warna" class="form-control" id="" placeholder="warna">
+                                            <input type="text" name="warna" class="form-control <?php echo form_error('warna') ? 'is-invalid' : '' ?>" id="" placeholder="warna">
+                                            <div class="invalid-feedback">
+                                                <?php echo form_error('warna') ?>
+                                            </div>
                                         </div>
 
                                         <div class="form-group">
                                             <label for="jenisKelamin">Jenis Kelamin</label>
                                             <div class="radio">
-                                                <input type="radio" name="jenisKelamin" value="0">
+                                                <input type="radio" name="jenisKelamin" class=" <?php echo form_error('jenisKelamin') ? 'is-invalid' : '' ?>" value="0">
                                                 <label style="margin-left:0.1cm">Jantan</label>
-
                                             </div>
                                             <div class="radio">
-                                                <input type="radio" name="jenisKelamin" value="1">
+                                                <input type="radio" name="jenisKelamin" class=" <?php echo form_error('jenisKelamin') ? 'is-invalid' : '' ?>" value="1">
                                                 <label style="margin-left:0.1cm">Betina</label>
                                             </div>
+                                        </div>
+                                        <div class="invalid-feedback">
+                                            <?php echo form_error('jenisKelamin') ?>
                                         </div>
                                     </form>
                                     <div class="modal-footer">
@@ -160,7 +178,7 @@
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <button class="btn btn-secondary" id="myBtn">Tambah Hewan Baru</button>
+                    <button class="btn btn-secondary" href="<?php echo base_url(); ?>admin/formDataHewan"id="myBtn">Tambah Hewan Baru</button>
                     <button class="btn btn-secondary">Simpan</button>
                 </div>
             </div>
@@ -190,12 +208,13 @@
 
 <?php
 
-function gender($code){
-  if($code==1){
-    return "Betina";
-  }else{
-    return "Jantan";
-  }
+function gender($code)
+{
+    if ($code == 1) {
+        return "Betina";
+    } else {
+        return "Jantan";
+    }
 }
 
 ?>
