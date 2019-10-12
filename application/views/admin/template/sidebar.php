928af7bce2100bd8fa4edfa7abff01324a6ce4e1
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-12 14:12:23 +0700

    Update sidebar.php

--- application/views/admin/template/sidebar.php
+++ application/views/admin/template/sidebar.php
@@ -100,27 +100,17 @@
           <i class="fas fa-fw fa-table"></i>
           <span>Data Pegawai</span></a>
       </li>
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Laporan</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Login Screens:</h6>
-          <a class="dropdown-item" href="login.html">Login</a>
-          <a class="dropdown-item" href="register.html">Register</a>
-          <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
-          <div class="dropdown-divider"></div>
-          <h6 class="dropdown-header">Other Pages:</h6>
-          <a class="dropdown-item" href="404.html">404 Page</a>
-          <a class="dropdown-item" href="blank.html">Blank Page</a>
-        </div>
-      </li> -->
+  
       <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>admin/laporan">
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Laporan</span></a>
       </li>
       
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>dokter/listRekamMedis">
+          <i class="fas fa-fw fa-file"></i>
+          <span>Rekam Medis</span></a>
+      </li>
       
     </ul>
