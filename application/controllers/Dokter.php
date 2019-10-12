892cee718f50049f46dd3854125d36244a2b78b1
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-12 14:12:19 +0700

    Update Dokter.php

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -1,5 +1,42 @@
 <?php
 
 class Dokter extends CI_Controller{
-    
+    public function __construct() {
+		parent::__construct();
+        $this->load->model("m_dokter");
+        $this->load->library('form_validation');
+
+
+		//If not login, cannot access this.
+		// $data = $this->session->has_userdata('logged_in');
+		// if(!$data){
+		// 	redirect('admin');
+		// }
+	}
+    /*
+    * REKAM MEDIS DOKTER
+    */
+
+    //lihat rekam medis
+    public function listRekamMedis()
+    {
+        
+        $data['title'] = 'Rekam Medis';
+        $data["data_hewan"] = $this->m_dokter->getHewan();
+        $this->load->view('admin/template/sidebar', $data);
+        $this->load->view('dokter/rekammedis/v_listRekamMedis');
+        $this->load->view('admin/template/sidebarfooter');
+    }
+
+    //lihat detail rekam medis
+    public function detailMedis($idHewan=null)
+    {
+        
+        $data['title'] = 'Rekam Medis';
+        $data["data_pet"] = $this->m_dokter->getHewan($idHewan);
+        $data["data_medis"] = $this->m_dokter->getRekamMedis($idHewan);
+        $this->load->view('admin/template/sidebar', $data);
+        $this->load->view('dokter/rekammedis/detail/v_detailMedis');
+        $this->load->view('admin/template/sidebarfooter');
+    }
 }
