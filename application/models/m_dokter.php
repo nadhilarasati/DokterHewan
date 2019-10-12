4bd6d5f5ba72eee89c60ffe520e047e14487e5bb
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-12 15:17:09 +0700

    Update m_dokter.php

--- application/models/m_dokter.php
+++ application/models/m_dokter.php
@@ -2,18 +2,22 @@
 
 class m_dokter extends CI_Model{
     
-    public function getHewan(){
-        return $this->db->get("daftar_hewan")->result();
+    public function getHewanById($idHewan){
+        //$this->db->where('idHewan', $idHewan);
+        return $this->db->get("daftar_hewan")->row();
     }
 
-    // public function getHewanById($idHewan)
-    // {
-    //     $this->db->where('idPemilik', $idHewan);
-    //     return $this->db->get('daftar_hewan')->result();
-    // }
+    public function getHewan()
+    {
+        return $this->db->get('daftar_hewan')->result();
+    }
 
     public function getRekamMedis($idRekamMedis){
         $this->db->where('idHewan', $idRekamMedis);
         return $this->db->get("rekam_medis")->result();
     }
+
+    // public function getPemilik(){
+    //     return $this->db->get("pemilik_peliharaan")->result();
+    // }
 }
