e98726f27cecab35f8d9a4f5f32bf59e3b30a259
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-21 11:53:14 +0700

    dompdf

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -9,11 +9,21 @@ class Laporan extends CI_Controller{
     }
 
     
-    function penerima_bantuan(){
-        $dompdf = new DOMPDF();
-        $dompdf->AddPage();
-        $dompdf->SetFont('Arial','B',16);
-        $dompdf->Cell(40,10,'Hello World!');
-        $dompdf->Output();
+    public function laporan_pdf(){
+
+        $data = array(
+            "dataku" => array(
+                "nama" => "Petani Kode",
+                "url" => "http://petanikode.com"
+            )
+        );
+    
+        $this->load->library('pdf');
+    
+        $this->pdf->setPaper('A4', 'potrait');
+        $this->pdf->filename = "laporan-petanikode.pdf";
+        $this->pdf->load_view('laporan/laporan_pdf', $data);
+    
+    
     }
 }
