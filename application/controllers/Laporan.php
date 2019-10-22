f2c40debd9fdd572f94306f6153221c11b26f7dc
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-22 15:49:59 +0700

    Update Laporan.php

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -4,26 +4,30 @@
 
 class Laporan extends CI_Controller{
 
-    function __construct(){
+    function __construct() {
         parent::__construct();
-    }
-
-    
-    public function laporan_pdf(){
-
-        $data = array(
-            "dataku" => array(
-                "nama" => "Petani Kode",
-                "url" => "http://petanikode.com"
-            )
-        );
-    
         $this->load->library('pdf');
+    }
     
-        $this->pdf->setPaper('A4', 'potrait');
-        $this->pdf->filename = "laporan-petanikode.pdf";
-        $this->pdf->load_view('laporan/laporan_pdf', $data);
-    
-    
+    function index(){
+        $pdf = new FPDF('l','mm','A5');
+        // membuat halaman baru
+        $pdf->AddPage();
+        // setting jenis font yang akan digunakan
+        $pdf->SetFont('Arial','B',16);
+        // mencetak string 
+        $pdf->Cell(190,7,'SEKOLAH MENENGAH KEJURUSAN NEEGRI 2 LANGSA',0,1,'C');
+        $pdf->SetFont('Arial','B',12);
+        $pdf->Cell(190,7,'DAFTAR SISWA KELAS IX JURUSAN REKAYASA PERANGKAT LUNAK',0,1,'C');
+        // Memberikan space kebawah agar tidak terlalu rapat
+        $pdf->Cell(10,7,'',0,1);
+        $pdf->SetFont('Arial','B',10);
+        $pdf->Cell(20,6,'NIM',1,0);
+        $pdf->Cell(85,6,'NAMA MAHASISWA',1,0);
+        $pdf->Cell(27,6,'NO HP',1,0);
+        $pdf->Cell(25,6,'TANGGAL LHR',1,1);
+        $pdf->SetFont('Arial','',10);
+        
+        $pdf->Output();
     }
 }
