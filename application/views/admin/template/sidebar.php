d5e5fe486a18965c3f71251c354378f8df831dff
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-24 09:58:08 +0700

    Update sidebar.php

--- application/views/admin/template/sidebar.php
+++ application/views/admin/template/sidebar.php
@@ -67,7 +67,7 @@
     </button>
 
     <!-- Navbar Search -->
-    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+    <!-- <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <div class="input-group">
         <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
         <div class="input-group-append">
@@ -76,7 +76,7 @@
           </button>
         </div>
       </div>
-    </form>
+    </form> -->
 
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
