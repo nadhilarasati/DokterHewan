61a0ed05f6f9991b53f6575b08175c606a94e61a
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-24 20:36:31 +0700

    Update v_loginPublic.php

--- application/views/public/login/v_loginPublic.php
+++ application/views/public/login/v_loginPublic.php
@@ -1,77 +1,4 @@
 <div class="bg-img">
-  <form action="/action_page.php" class="container">
-    <h1 style="margin-bottom:50px">Login</h1>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit" class="btn">Login</button>
-  </form>
-</div>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-.bg-img {
-  /* The image used */
-  background-image: url("../img/login.jpg");
-
-  height: 100%;
-  
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-}
-
-/* Add styles to the form container */
-.container {
-  position: absolute;
-  right: 20px;
-  margin: 20px;
-  max-width: 300px;
-  padding: 16px;
-  background-color: white;
-}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit button */
-.btn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.btn:hover {
-  opacity: 1;
-}
-</style>
-
-<!-- <div class="bg-img">
     <div class="container py-5">
         <div class="row">
             <div class="col-md-12">
@@ -84,13 +11,13 @@ input[type=text]:focus, input[type=password]:focus {
                             <div class="card-body">
                                 <form class="form" role="form" autocomplete="off" id="formLogin" novalidate="" method="POST">
                                     <div class="form-group">
-                                        <label for="uname1">Username</label>
-                                        <input type="text" class="form-control form-control-lg rounded-0" name="uname1" id="uname1" required="">
+                                        <label for="uname1">Email</label>
+                                        <input name="email "type="text" class="form-control form-control-lg rounded-0" name="uname1" id="uname1" required="">
                                         <div class="invalid-feedback">Oops, you missed this one.</div>
                                     </div>
                                     <div class="form-group">
                                         <label>Password</label>
-                                        <input type="password" class="form-control form-control-lg rounded-0" id="pwd1" required="" autocomplete="new-password">
+                                        <input name="password" type="password" class="form-control form-control-lg rounded-0" id="pwd1" required="" autocomplete="new-password">
                                         <div class="invalid-feedback">Enter your password too!</div>
                                     </div>
                                     <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Login</button>
@@ -102,4 +29,4 @@ input[type=text]:focus, input[type=password]:focus {
             </div>
         </div>
     </div>
-</div> -->
+</div>
