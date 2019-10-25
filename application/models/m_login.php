14baca755baac7d892acded1dc5e91c53fb6ce68
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-25 18:50:09 +0700

    login

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -3,5 +3,12 @@
 class M_login extends CI_Model{	
 	function cek_login($table,$where){		
 		return $this->db->get_where($table,$where);
-	}	
+    }	
+    
+    function validate($email,$password){
+        $this->db->where('email',$email);
+        $this->db->where('password',$password);
+        $result = $this->db->get('staff_klinik',1);
+        return $result;
+      }
 }
