752bae5793befc42dddd315049ca11e895c7d923
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-26 15:45:49 +0700

    Update v_regPasien.php

--- application/views/admin/datapasien/form/v_regPasien.php
+++ application/views/admin/datapasien/form/v_regPasien.php
@@ -1,6 +1,6 @@
-<div id="content-wrapper">
+<div id="content-wrapper" style="float:right; width:80vw">
     <div class="container-fluid">
-        <h2 class="text-center" style="margin-bottom:1cm; margin-top:1cm">Registrasi Pasien Baru</h2>
+        <h2 class="text-center" style="margin-bottom:1cm; margin-top:3cm">Registrasi Pasien Baru</h2>
         <form class="data-pasien" action="<?php site_url('admin/formDataPasien'); ?>" method="post" enctype="multipart/form-data">
             <div class="row">
 
