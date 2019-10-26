501709ce48629b79a3c711aafd452a9624e81044
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-26 15:45:59 +0700

    Update v_listLaporan.php

--- application/views/admin/laporan/v_listLaporan.php
+++ application/views/admin/laporan/v_listLaporan.php
@@ -1,9 +1,9 @@
-<div id="content-wrapper">
+<div id="content-wrapper" style="float:right; width:80vw">
 
       <div class="container-fluid">
 
         <!-- DataTables Example -->
-        <div class="card">
+        <div class="card" style="margin-top: 80px;">
           <div class="card-header">
             <a class="btn btn-secondary" href="<?php echo base_url(); ?>laporan/index"><i class="fas fa-download"></i>Download Report</a>
           </div>
