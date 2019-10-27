873535ea7ed8a071ff0eb0b40fa41a992b4017e6
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-27 20:06:29 +0700

    Update Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -41,6 +41,14 @@ class Pages extends CI_Controller
         $this->load->view('templates/footer');
     }
 
+    public function profile()
+    {
+        $data['title'] = 'Profile';
+        $this->load->view('templates/header', $data);
+        $this->load->view('public/profile/v_userProfile');
+        $this->load->view('templates/footer');
+    }
+
     // ADMIN PAGE
     
 }
