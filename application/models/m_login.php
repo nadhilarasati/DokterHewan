f107d1f50e03d9df619b4f4b2f0f9ea2664b9b29
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-28 20:18:55 +0700

    Update m_login.php

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -1,14 +1,17 @@
-<?php 
- 
-class M_login extends CI_Model{	
-	function cek_login($table,$where){		
-		return $this->db->get_where($table,$where);
-    }	
-    
-    function validate($email,$password){
-        $this->db->where('email',$email);
-        $this->db->where('password',$password);
-        $result = $this->db->get('staff_klinik',1);
-        return $result;
-      }
+<?php
+
+class M_login extends CI_Model
+{
+  function cek_login($table, $where)
+  {
+    return $this->db->get_where($table, $where);
+  }
+
+  function validate($email, $password)
+  {
+    $this->db->where('email', $email);
+    $this->db->where('password', $password);
+    $result = $this->db->get('staff_klinik', 1);
+    return $result;
+  }
 }
