987cad15726d92c904a7f02e358e85ac7308c13e
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-28 20:18:51 +0700

    Update UserProfile.php

--- application/controllers/UserProfile.php
+++ application/controllers/UserProfile.php
@@ -1,19 +1,33 @@
 <?php
 
-class UserProfile extends CI_Controller{
+class UserProfile extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
         $this->load->model("m_user");
         $this->load->library('form_validation');
-        // if ($this->session->userdata('logged_in') !== TRUE) {
-        //     redirect('login');
+        if($this->session->userdata('status') != "login"){
+			redirect(base_url("pages/login"));
+		}
+        // if ($this->session->userdata('logged_in') == TRUE) {
+        //     $this->load->view('templates/headerLogin');
+        //     $this->load->view('public/profile/v_userProfile');
+        //     $this->load->view('templates/footer');
+        // } else {
+        //     redirect('pages/login');
         // }
     }
 
-    public function seeProfile(){
+
+    public function afterLogin(){
+        
+    }
+    
+    public function seeProfile()
+    {
         $data['title'] = 'Profile';
-        $this->load->view('templates/header', $data);
+        $this->load->view('templates/headerLogin', $data);
         $this->load->view('public/profile/v_userProfile');
         $this->load->view('templates/footer');
     }
