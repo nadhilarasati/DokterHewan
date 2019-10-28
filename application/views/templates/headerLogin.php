9ec14bafcdc29a8e22fcad257d3d9549845e707a
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-28 20:19:05 +0700

    Create headerLogin.php

---
+++ application/views/templates/headerLogin.php
@@ -0,0 +1,92 @@
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>lib/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>lib/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>lib/css/responsive.css">
+
+
+
+
+    <link href="<?php echo base_url(); ?>lib/font/comfortaa.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <script src="<?php echo base_url(); ?>lib/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?php echo base_url(); ?>lib/js/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>lib/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>lib/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>lib/js/custom.js"></script>
+
+
+    <!-- <link rel="stylesheet" type="text/css" href="../../lib/css/bootstrap/bootstrap.min.css">
+
+    <script src="../../lib/jquery-3.3.1.slim.min.js"></script>
+    <script src="../../lib/popper.min.js"></script>
+    <script src="../../lib/bootstrap.min.js"></script> -->
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark default-color">
+        <a class="navbar-brand glow" href="#">Klinik Nyanko</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url() ?>">HOME
+                        <!-- <span class="sr-only">(current)</span> -->
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="<?php echo base_url('pages/aboutus') ?>">TENTANG KAMI</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('pages/facilities') ?>">FASILITAS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">EVENTS</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <button class="dropbtn">Dropdown
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="#">Lihat Profile</a>
+                            <a href="#">Logout</a>
+                        </div>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('pages/profile') ?>">PROFILE</a>
+                </li> -->
+
+            </ul>
+            <ul class="navbar-nav right-item">
+                <!-- <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </li> -->
+
+            </ul>
+        </div>
+    </nav>
+    </div>
