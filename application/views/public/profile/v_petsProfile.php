2f62b3fa999d7a7e64924e59b217922bdafbb209
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-29 22:39:08 +0700

    Create v_petsProfile.php

---
+++ application/views/public/profile/v_petsProfile.php
@@ -0,0 +1,45 @@
+<div class="bc" style="float:right; width:70vw; height:100%">
+    <div class="card" style="margin-top:50px; margin-left:30px; margin-right:30px; margin-bottom: 30px; background-color: #ffcccc">
+        <h4 style="margin-left:20px; margin-top:20px">Ini adalah data hewan peliharaan anda!</h4>
+        <p style="margin-left:20px">Anda hanya bisa mengunggah foto hewan peliharaan anda. Jika terjadi kesalahan data lain, silahkan hubungi pihak klinik.</p>
+        <div style="margin-left:20px; margin-bottom:20px; width: 105%">
+            <table style="width: 90%">
+                <thead>
+                    <tr class="text-center">
+                        <th>Nama Hewan</th>
+                        <th>Jenis Hewan</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Ras</th>
+                        <th>Warna</th>
+                        <th>Status</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a class="btn btn-primary" style="background-color:black" href="<?php echo site_url('UserProfile/seeDetail')?>"><i class="fa fa-file"></i></a></td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    table,
+    th,
+    td {
+        border: 2px solid black;
+    }
+</style>
