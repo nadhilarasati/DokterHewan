5e8af4648c4008ad00defc55b63d759ec349be67
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-29 15:47:42 +0700

    Update headerLogin.php

--- application/views/templates/headerLogin.php
+++ application/views/templates/headerLogin.php
@@ -56,13 +56,14 @@
                 </li>
                 <li class="nav-item">
                     <div class="dropdown">
-                        <button class="dropbtn">Dropdown
+                        <button class="dropbtn">PROFILE
                             <i class="fa fa-caret-down"></i>
                         </button>
                         <div class="dropdown-content">
                             <a href="#">Lihat Profile</a>
                             <a href="#">Logout</a>
                         </div>
+                    </div>
                 </li>
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="<?php echo base_url('pages/profile') ?>">PROFILE</a>
@@ -90,3 +91,67 @@
         </div>
     </nav>
     </div>
+</body>
+
+<style>
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
