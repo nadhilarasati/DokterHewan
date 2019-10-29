fe3b3ec9c4448c2d5a875ac5b473018fe8f7718c
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-29 22:39:06 +0700

    Create v_detail.php

---
+++ application/views/public/profile/v_detail.php
@@ -0,0 +1,100 @@
+<div class="bc" style="float:right; width:70vw">
+    <div class="card" style="margin-top:50px; margin-left:30px; margin-right:30px; margin-bottom: 30px; background-color: #ffcccc">
+        <h1 style="margin-left:20px; margin-top:20px">Halo!</h1>
+        <h4 style="margin-left:20px; margin-top:30px">Data Hewan</h4>
+        <div class="row">
+            <div class="col-md-4">
+                <div style="margin-left:20px; margin-top:20px">
+                    <img src="../lib/img/profile3.jpg" style="width:100%">
+                    <div style="margin-top:20px">
+                        <button>Ubah Foto</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div style="margin-top:20px">
+                    <div class="form-group row">
+                        <label for="namaHewan" class="col-sm-4 col-form-label">Nama Hewan</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="namaHewan" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="jenisHewan" class="col-sm-4 col-form-label">Jenis Hewan</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="jenisHewan" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="tanggalLahir" class="col-sm-4 col-form-label">Tanggal Lahir</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="tanggalLahir" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="ras" class="col-sm-4 col-form-label">Ras</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="ras" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="warna" class="col-sm-4 col-form-label">Warna</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="warna" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="status" class="col-sm-4 col-form-label">Status</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="status" class="form-control" id="" readonly>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h4 style="margin-left:20px; margin-top:30px">Rekam Medis</h4>
+        <p style="margin-left:20px">Ini adalah histori berobat hewan peliharaan anda!</p>
+        <div style="margin-left:20px; margin-bottom:20px; width: 105%">
+            <table style="width: 90%">
+                <thead>
+                    <tr class="text-center">
+                        <th>Tanggal</th>
+                        <th>Gejala Klinis</th>
+                        <th>Berat Badan</th>
+                        <th>Suhu Badan</th>
+                        <th>Diagnosa</th>
+                        <th>Tindakan</th>
+                        <th>Obat</th>
+                        <th>Diperiksa Oleh</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <td>aa</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    table,
+    th,
+    td {
+        border: 2px solid black;
+    }
+</style>
