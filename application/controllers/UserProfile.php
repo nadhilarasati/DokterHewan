33ffeef1acb58a35c8385cb2d852756284138f69
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-10-30 19:31:49 +0700

    Update UserProfile.php

--- application/controllers/UserProfile.php
+++ application/controllers/UserProfile.php
@@ -18,6 +18,35 @@ class UserProfile extends CI_Controller
         //     redirect('pages/login');
         // }
     }
+
+    //Navbar Login
+
+    public function index()
+    {
+
+        $data['title'] = 'Home';
+        $this->load->view('templates/headerLogin', $data);
+        $this->load->view('public/home/v_home');
+        $this->load->view('templates/footer');
+    }
+
+    public function aboutus()
+    {
+        $data['title'] = 'About Us';
+        $this->load->view('templates/headerLogin', $data);
+        $this->load->view('public/aboutus/v_aboutus');
+        $this->load->view('templates/footer');
+    }
+
+    public function facilities()
+    {
+        $data['title'] = 'Facilities';
+        $this->load->view('templates/headerLogin', $data);
+        $this->load->view('public/facilities/v_facilities');
+        $this->load->view('templates/footer');
+    }
+
+    //profile
     
     public function seeProfile()
     {
