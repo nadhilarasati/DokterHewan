51ab227b0a107f6b7d6ba70244a6617ddad9f0a3
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-01 22:02:23 +0700

    Update UserProfile.php

--- application/controllers/UserProfile.php
+++ application/controllers/UserProfile.php
@@ -6,6 +6,7 @@ class UserProfile extends CI_Controller
     {
         parent::__construct();
         $this->load->model("m_user");
+        $this->load->library('session');
         $this->load->library('form_validation');
         if ($this->session->userdata('logged_in') !== TRUE) {
             redirect('pages/login');
@@ -16,8 +17,8 @@ class UserProfile extends CI_Controller
 
     public function index()
     {
-
         $data['title'] = 'Home';
+        $data['data_pemilik'] = $this->m_user->getPemilikById($this->session->userdata('email'));
         $this->load->view('templates/headerLogin', $data);
         $this->load->view('public/home/v_home');
         $this->load->view('templates/footer');
@@ -44,7 +45,7 @@ class UserProfile extends CI_Controller
     public function seeProfile()
     {
         $data['title'] = 'Profile';
-        $data['data_pemilik'] = $this->m_user->getPemilikById($this->session->user_data('email'));
+        $data['data_pemilik'] = $this->m_user->getPemilikById($this->session->userdata('email'));
         $this->load->view('templates/headerLogin', $data);
         $this->load->view('templates/sidebarProfile', $data);
         $this->load->view('public/profile/v_userProfile');
@@ -54,17 +55,20 @@ class UserProfile extends CI_Controller
     public function seePets()
     {
         $data['title'] = 'Profile';
+        $data['data_hewan'] = $this->m_user->getHewanById($this->session->userdata('email'));
         $this->load->view('templates/headerLogin', $data);
         $this->load->view('templates/sidebarProfile', $data);
         $this->load->view('public/profile/v_petsProfile');
         $this->load->view('templates/footer');
     }
 
-    public function seeDetail()
+    public function seeDetail($idHewan=null)
     {
         $data['title'] = 'Profile';
+        $data['data_pet'] = $this->m_user->getHewan($idHewan);
+        $data['data_medis'] = $this->m_user->getRekamMedis($idHewan);
         $this->load->view('templates/headerLogin', $data);
-        $this->load->view('templates/sidebarProfile', $data);
+        $this->load->view('templates/sidebarMedis', $data);
         $this->load->view('public/profile/v_detail');
         $this->load->view('templates/footer');
     }
