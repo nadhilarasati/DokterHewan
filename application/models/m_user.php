b22cd22fc3a872ae30e90a184fc5e43947176e81
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-01 22:02:29 +0700

    Update m_user.php

--- application/models/m_user.php
+++ application/models/m_user.php
@@ -1,10 +1,35 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 class m_user extends CI_Model{
-    
+    public function getIdByEmail($email){
+        $this->db->select('idPemilik');
+        $this->db->from('pemilik_hewan');
+        $this->db->where('email', $email);
+        $query = $this->db->get()->row();
+        return $query;
+    }
     public function getPemilikById($idPemilik)
     {
-        $this->db->where('idPemilik', $idPemilik);
+        $this->db->where('email', $idPemilik);
         return $this->db->get('pemilik_hewan')->row();
     }
+
+    public function getHewanById($idHewan)
+    {
+        $this->db->where('idPemilik', $this->getIdByEmail($idHewan)->idPemilik);
+        return $this->db->get('daftar_hewan')->result();
+    }
+
+    public function getHewan($idHewan)
+    {
+        $this->db->where('idHewan', $idHewan);
+        return $this->db->get("daftar_hewan")->row();
+    }
+
+    public function getRekamMedis($idRekamMedis)
+    {
+        $this->db->where('idHewan', $idRekamMedis);
+        return $this->db->get("rekam_medis")->result();
+    }
+
 }
