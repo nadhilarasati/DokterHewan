f7b160dad256c3ae9ee77a07949857af5df9754a
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-04 12:18:35 +0700

    Update UserProfile.php

--- application/controllers/UserProfile.php
+++ application/controllers/UserProfile.php
@@ -41,7 +41,7 @@ class UserProfile extends CI_Controller
     }
 
     //profile
-    
+
     public function seeProfile()
     {
         $data['title'] = 'Profile';
@@ -62,7 +62,7 @@ class UserProfile extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-    public function seeDetail($idHewan=null)
+    public function seeDetail($idHewan = null)
     {
         $data['title'] = 'Profile';
         $data['data_pet'] = $this->m_user->getHewan($idHewan);
@@ -75,35 +75,34 @@ class UserProfile extends CI_Controller
 
     public function ubahPassword()
     {
-        $this->form_validation->set_rules('passLama', 'Password Lama', 'trim|required|min_length[5]|max_length[12]');
-        $this->form_validation->set_rules('passBaru', 'Password Baru', 'trim|required|min_length[5]|max_length[12]');
-        $this->form_validation->set_rules('passKonf', 'Password Konfirmasi', 'trim|required|min_length[5]|max_length[12]');
- 
+        $this->form_validation->set_rules('passLama', 'Password Lama', 'trim|required');
+        $this->form_validation->set_rules('passBaru', 'Password Baru', 'trim|required|min_length[5]|max_length[12]|matches[passKonf]');
+        $this->form_validation->set_rules('passKonf', 'Password Konfirmasi', 'trim|required|min_length[5]|max_length[12]|matches[passBaru]');
+
         $email = $this->session->userdata('email');
         if ($this->form_validation->run() == true) {
             if (password_verify($this->input->post('passLama'), $this->session->userdata('password'))) {
                 if ($this->input->post('passBaru') != $this->input->post('passKonf')) {
                     $this->session->set_flashdata('msg', show_err_msg('Password Baru dan Konfirmasi Password harus sama'));
-                    redirect('UserProfile/seePets');
+                    redirect('UserProfile/seeProfile');
                 } else {
-                    $data = ['password' => get_hash($this->input->post('passBaru'))];
-                    $result = $this->Auth_model->update($data, $email);
+                    $data = ['password' => $this->input->post('passBaru')];
+                    $result = $this->m_user->editPassword($email);
                     if ($result > 0) {
-                        $this->m_user->editPassword($data);
                         $this->session->set_flashdata('msg', show_succ_msg('Password Berhasil diubah'));
                         redirect('UserProfile/seeProfile');
                     } else {
                         $this->session->set_flashdata('msg', show_err_msg('Password Gagal diubah'));
-                        redirect('UserProfile/facilities');
+                        redirect('UserProfile/seeProfile');
                     }
                 }
             } else {
                 $this->session->set_flashdata('msg', show_err_msg('Password Salah'));
-                redirect('UserProfile/aboutus');
+                redirect('UserProfile/seeProfile');
             }
         } else {
             $this->session->set_flashdata('msg', show_err_msg(validation_errors()));
-            redirect('UserProfile/index');
+            redirect('UserProfile/seeProfile');
         }
     }
 }
