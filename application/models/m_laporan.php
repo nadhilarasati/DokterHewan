6ef5802eb7d9dfe9b3ed6f2f09a4ea05f46aba2e
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-05 19:43:43 +0700

    Update m_laporan.php

--- application/models/m_laporan.php
+++ application/models/m_laporan.php
@@ -2,6 +2,16 @@
 
 class m_laporan extends CI_Model
 {
+    //laporan
+    public function getListLaporan()
+    {
+        return $this->db->get("laporan_kunjungan_view")->result();
+    }
 
-    
+    public function detail($jenis, $tanggal)
+    {
+        $this->db->where('jenis', $jenis);
+        $this->db->where('tanggal', $tanggal);
+        return $this->db->get("detail_laporan_view")->result();
+    }
 }
