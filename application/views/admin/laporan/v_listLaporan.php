da2bf816b99340f18a7af818b624d003f54f64f5
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-07 15:34:52 +0700

    Update v_listLaporan.php

--- application/views/admin/laporan/v_listLaporan.php
+++ application/views/admin/laporan/v_listLaporan.php
@@ -5,7 +5,7 @@
     <!-- DataTables Example -->
     <div class="card" style="margin-top: 80px;">
       <div class="card-header">
-        <a class="btn btn-secondary" id="myBtn"><i class="fas fa-download"></i>Unduh Laporan</a>
+        <a class="btn btn-secondary" id="myBtn" style="color:white"><i class="fas fa-download"></i>Unduh Laporan</a>
       </div>
       <div class="card-body">
         <div class="table-responsive">
@@ -19,14 +19,14 @@
               </tr>
             </thead>
             <tbody>
-            <?php foreach ($laporan as $l) : ?>
-              <tr class="text-center">
+              <?php foreach ($laporan as $l) : ?>
+                <tr class="text-center">
                   <td><?php echo $l->tanggal ?></td>
                   <td><?php echo $l->jenis ?></td>
                   <td><?php echo $l->Jumlah ?></td>
-                  <td><a class="btn btn-primary" href="<?php echo site_url('Laporan/detailLaporan/'.$l->jenis.'/'.$l->tanggal); ?>"><i class="fas fa-pen"></i></a></td>
-              </tr>
-            <?php endforeach ?>
+                  <td><a class="btn btn-primary" href="<?php echo site_url('Laporan/detailLaporan/' . $l->jenis . '/' . $l->tanggal); ?>"><i class="fas fa-pen"></i></a></td>
+                </tr>
+              <?php endforeach ?>
             </tbody>
           </table>
         </div>
@@ -35,10 +35,54 @@
   </div>
 </div>
 
+<!-- Modal 'unduh' button-->
+<div class="modal fade" id="myModal" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content" style="padding:40px 50px;">
+      <div class="modal-header">
+        <h4 class="modal-title">Unduh Laporan</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <?php echo form_open('Laporan/laporanTanggal') ?>
+
+      <div class="form-group" style="margin-top:0.5cm">
+        <label for="tanggalAwal">Tanggal Awal</label>
+        <input type="date" name="tanggalAwal" class="tanggal form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="tanggalAkhir">Tanggal Akhir</label>
+        <input type="date" name="tanggalAkhir" class="tanggal form-control">
+      </div>
+
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-secondary" href="<?php echo base_url('Laporan/laporanTanggal') ?>">Unduh</button>
+        <!--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
+      </div>
+      <?php echo form_close(); ?>
+
+    </div>
+  </div>
+</div>
+
 <script>
   $(document).ready(function() {
     $("#myBtn").click(function() {
       $("#myModal").modal();
+    });
+  });
+</script>
+
+<script src="js/jquery.min.2.0.2.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+<script src="datepicker/bootstrap-datepicker.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.tanggal').datepicker({
+      format: "dd-mm-yyyy",
+      autoclose: true
     });
   });
 </script>
