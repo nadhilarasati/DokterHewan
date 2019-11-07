57db0004614cbc4652fe39c4834579b7e9930ca7
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-07 15:34:44 +0700

    Update Laporan.php

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -24,13 +24,43 @@ class Laporan extends CI_Controller
         $this->load->view('admin/template/sidebarfooter');
     }
 
-    public function detailLaporan($jenis,$tanggal)
+    public function detailLaporan($jenis, $tanggal)
     {
         $data['title'] = 'Detail Laporan';
-        $data['detail'] = $this->m_laporan->detail($jenis,$tanggal);
+        $data['detail'] = $this->m_laporan->detail($jenis, $tanggal);
         $this->load->view('admin/template/sidebar', $data);
         $this->load->view('admin/laporan/v_detailLaporan', $data);
         $this->load->view('admin/template/sidebarfooter');
+    }
+
+    function laporanTanggal()
+    {
+
+        $pdf = new FPDF('p', 'mm', 'A4');
+        // membuat halaman baru
+        $pdf->AddPage();
+        // setting jenis font yang akan digunakan
+        $pdf->SetFont('Arial', 'B', 16);
+        // mencetak string 
+        $pdf->Cell(190, 25, 'REKAP LAPORAN KUNJUNGAN KLINIK NYANKO', 0, 1, 'C');
+
+        // Memberikan space kebawah agar tidak terlalu rapat
+        $pdf->Cell(10, 15, '', 0, 1);
+        $pdf->SetFont('Arial', 'B', 10);
+        $pdf->Cell(80, 6, 'Tanggal', 1, 0, 'C');
+        $pdf->Cell(55, 6, 'Jenis Hewan', 1, 0, 'C');
+        $pdf->Cell(55, 6, 'Jumlah Kunjungan', 1, 1, 'C');
+        $laporan = $this->m_laporan->getTanggal();
+
+        foreach ($laporan as $l) {
+            $pdf->SetFont('Arial', '', 10);
+            $pdf->Cell(80, 6, $l->tanggal, 1, 0, 'C');
+            $pdf->Cell(55, 6, $l->jenis, 1, 0, 'C');
+            $pdf->Cell(55, 6, $l->Jumlah, 1, 1, 'C');
+
+        }
+
+        $pdf->Output();
     }
 
 
@@ -38,7 +68,7 @@ class Laporan extends CI_Controller
 
     function formulirRegistrasi()
     {
-        
+
         $pdf = new FPDF('p', 'mm', 'A4');
         // membuat halaman baru
         $pdf->AddPage();
@@ -59,55 +89,62 @@ class Laporan extends CI_Controller
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(195, 10, 'No. KTP: ...................................', 0, 1, 'L');
         $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(195, 10, 'Nama Pemilik Hewan: ...................................', 0, 1, 'L') ;
+        $pdf->Cell(195, 10, 'Nama Pemilik Hewan: ...................................', 0, 1, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(195, 10, 'Alamat: ...................................', 0, 1, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(195, 10, 'Telepon: ...................................', 0, 1, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(195, 10, 'Email: ...................................', 0, 1, 'L');
-         //isi formulir data pemilik
-         $pdf->SetFont('Arial', 'B', 14);
-         $pdf->Cell(195, 20, 'DATA HEWAN PELIHARAAN', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Nama Hewan: ...................................', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Jenis Hewan: ...................................', 0, 1, 'L') ;
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Jenis Kelamin: ...................................', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Tanggal Lahir: ...................................', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Ras: ...................................', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 10, 'Warna: ...................................', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 55, 'Bogor, ......', 0, 1, 'L');
-         $pdf->SetFont('Arial', '', 12);
-         $pdf->Cell(195, 20, '(Pemilik Hewan)', 0, 1, 'L');
+        //isi formulir data pemilik
+        $pdf->SetFont('Arial', 'B', 14);
+        $pdf->Cell(195, 20, 'DATA HEWAN PELIHARAAN', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Nama Hewan: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Jenis Hewan: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Jenis Kelamin: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Tanggal Lahir: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Ras: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 10, 'Warna: ...................................', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 55, 'Bogor, ......', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(195, 20, '(Pemilik Hewan)', 0, 1, 'L');
 
         $pdf->Output();
     }
 
-    function index()
+    function laporanKunjungan()
     {
-        $pdf = new FPDF('l', 'mm', 'A5');
+
+        $pdf = new FPDF('p', 'mm', 'A4');
         // membuat halaman baru
         $pdf->AddPage();
         // setting jenis font yang akan digunakan
         $pdf->SetFont('Arial', 'B', 16);
         // mencetak string 
-        $pdf->Cell(190, 7, 'SEKOLAH MENENGAH KEJURUSAN NEEGRI 2 LANGSA', 0, 1, 'C');
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(190, 7, 'DAFTAR SISWA KELAS IX JURUSAN REKAYASA PERANGKAT LUNAK', 0, 1, 'C');
+        $pdf->Cell(190, 25, 'REKAP LAPORAN KUNJUNGAN KLINIK NYANKO', 0, 1, 'C');
+
         // Memberikan space kebawah agar tidak terlalu rapat
-        $pdf->Cell(10, 7, '', 0, 1);
+        $pdf->Cell(10, 15, '', 0, 1);
         $pdf->SetFont('Arial', 'B', 10);
-        $pdf->Cell(20, 6, 'NIM', 1, 0);
-        $pdf->Cell(85, 6, 'NAMA MAHASISWA', 1, 0);
-        $pdf->Cell(27, 6, 'NO HP', 1, 0);
-        $pdf->Cell(25, 6, 'TANGGAL LHR', 1, 1);
-        $pdf->SetFont('Arial', '', 10);
+        $pdf->Cell(80, 6, 'Tanggal', 1, 0, 'C');
+        $pdf->Cell(55, 6, 'Jenis Hewan', 1, 0, 'C');
+        $pdf->Cell(55, 6, 'Jumlah Kunjungan', 1, 1, 'C');
+        $laporan = $this->m_laporan->getListLaporan();
+
+        foreach ($laporan as $l) {
+            $pdf->SetFont('Arial', '', 10);
+            $pdf->Cell(80, 6, $l->tanggal, 1, 0, 'C');
+            $pdf->Cell(55, 6, $l->jenis, 1, 0, 'C');
+            $pdf->Cell(55, 6, $l->Jumlah, 1, 1, 'C');
+
+        }
 
         $pdf->Output();
     }
