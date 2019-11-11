7a649aa21d0e6d1907d6056075178265a14b9e60
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-11 12:37:58 +0700

    Update sidebarFasilitas.php

--- application/views/templates/sidebarFasilitas.php
+++ application/views/templates/sidebarFasilitas.php
@@ -1,6 +1,16 @@
-<div class="side" style="float:left; width:30vw; height: 100%; position:fixed">
+<div class="side" style="float:left; width:25vw; height: 100%; position:fixed">
     <div style="margin-top:100px">
-        <h3 class="text-center">FASILITAS KLINIK NYANKO</h3>
+        <h4 class="text-center">FASILITAS KLINIK NYANKO</h4>
+        <div class="row">
+            <div class="col nav-item" style="margin-top:30px">
+                <a href="" style="padding:44%">VAKSIN</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col nav-item" style="margin-top:30px">
+                <a href="" style="padding:44%">VAKSIN</a>
+            </div>
+        </div>
     </div>
 </div>
 
