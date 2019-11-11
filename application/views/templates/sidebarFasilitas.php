4316fe7502ae36c8922e6f2f1ee677722e3d8486
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-11 11:52:50 +0700

    Create sidebarFasilitas.php

---
+++ application/views/templates/sidebarFasilitas.php
@@ -0,0 +1,11 @@
+<div class="side" style="float:left; width:30vw; height: 100%; position:fixed">
+    <div style="margin-top:100px">
+        <h3 class="text-center">FASILITAS KLINIK NYANKO</h3>
+    </div>
+</div>
+
+<style>
+    .side {
+        background-color: #ffcccc;
+    }
+</style>
