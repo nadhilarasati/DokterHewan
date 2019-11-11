2f3afe3b36a8445c1fd1c04d877f1a69d7140621
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-11 12:37:55 +0700

    Update v_facilities.php

--- application/views/public/facilities/v_facilities.php
+++ application/views/public/facilities/v_facilities.php
@@ -1,3 +1,15 @@
-<div style="float:right; width:70vw; height: 100%; background-color: rgb(255, 249, 229);">
+<div style="float:right; width:75vw; height: 100%; background-color: rgb(255, 249, 229);">
+    <div class="card" style="margin-top:50px; margin-left:30px; margin-right:30px; margin-bottom: 30px; background-color: #ffcccc">
+        <h4 style="margin-left:20px; margin-top:20px">Vaksin</h4>
+        <div class="row" style="margin-top:20px">
+            <div class="col-md-6">
+                <div style="margin-left:20px">
+                    <img src="../lib/img/vaksin.jpg" style="width:100%">
+                </div>
+            </div>
+            <div class="col-md-6">
 
+            </div>
+        </div>
+    </div>
 </div>
