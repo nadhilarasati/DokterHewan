1c9fb454c891f49e36e3d711cfe537b286ff7c95
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-11 12:46:22 +0700

    Update sidebarFasilitas.php

--- application/views/templates/sidebarFasilitas.php
+++ application/views/templates/sidebarFasilitas.php
@@ -1,14 +1,15 @@
 <div class="side" style="float:left; width:25vw; height: 100%; position:fixed">
-    <div style="margin-top:100px">
-        <h4 class="text-center">FASILITAS KLINIK NYANKO</h4>
+    <div class="fasilitas" style="margin-top:200px; margin-left:35px">
+        <h3>FASILITAS KLINIK NYANKO</h3>
         <div class="row">
-            <div class="col nav-item" style="margin-top:30px">
-                <a href="" style="padding:44%">VAKSIN</a>
+            <div class="col nav-item">
+                <a href="">VAKSIN</a>
             </div>
         </div>
+        |
         <div class="row">
-            <div class="col nav-item" style="margin-top:30px">
-                <a href="" style="padding:44%">VAKSIN</a>
+            <div class="col nav-item">
+                <a href="">VAKSIN</a>
             </div>
         </div>
     </div>
@@ -17,5 +18,23 @@
 <style>
     .side {
         background-color: #ffcccc;
+    }
+
+    .fasilitas {
+        background-color: rgb(0, 0, 0);
+        /* Fallback color */
+        background-color: rgba(0, 0, 0, 0.4);
+        /* Black w/opacity/see-through */
+        color: white;
+        font-weight: bold;
+        border: 3px solid #f1f1f1;
+        /* position: absolute; */
+        top: 50%;
+        left: 50%;
+        /* transform: translate(-50%, -50%); */
+        z-index: 2;
+        width: 80%;
+        padding: 20px;
+        text-align: center;
     }
 </style>
