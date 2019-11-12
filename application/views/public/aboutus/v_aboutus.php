771fae65f21e039c6f7478318ef03ecee5967cd6
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-12 11:36:59 +0700

    Update v_aboutus.php

--- application/views/public/aboutus/v_aboutus.php
+++ application/views/public/aboutus/v_aboutus.php
@@ -1,53 +1,15 @@
-<div class="about-us">
-    <div class="row desc-cont">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fa fa-book"></i>
-                    <h5 class="card-title">DESCRIPTION</h5>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fa fa-id-card"></i>
-                    <h5 class="card-title">CONTACT</h5>
-                </div>
-                <div class="card-body">
-                    
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </div>
+<div style="background-color: rgb(255, 249, 229); height:100%">
+    <div style="padding-top:50px; padding-left:20px">
+        <h2 class="text-center">TENTANG KAMI</h2>
     </div>
-    <div class="row activity">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fa fa-briefcase"></i>
-                    <h5 class="card-title">ACTIVITY</h5>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the </p>
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-md-6" style="padding-top:70px; padding-left:50px">
+            <img src="../lib/img/nyanko.jpg">
         </div>
-    </div>
-    <div class="row section">
-        <div class="col-md-6">
-            <img src="<?php echo base_url(); ?>lib/img/petshop.jpg">
-        </div>
-        <div class="col-md-6">
-            <div class="desc-activity">
-                <h5>Office Location</h5>
-                <p>
-            Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the
-            </p>
-            </div>
+        <div class="col-md-6" style="padding-top:70px; padding-right:50px">
+        <p>Klinik Nyanko berdiri pada tahun 2016. Klinik yang didirikan oleh Drh. Oktavia Pusparini ini dulu merupakan sebuah pet shop. 
+            
+        </p>
         </div>
     </div>
 </div>
