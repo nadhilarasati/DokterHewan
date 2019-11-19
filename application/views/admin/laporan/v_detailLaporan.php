7be901b212d8e705997f74b7a8ce736d0da91aff
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-19 13:03:30 +0700

    Update v_detailLaporan.php

--- application/views/admin/laporan/v_detailLaporan.php
+++ application/views/admin/laporan/v_detailLaporan.php
@@ -2,6 +2,8 @@
 
     <div class="container-fluid">
 
+    <h2 class="text-center"style="margin-top:3cm;margin-bottom:1cm">Detail Laporan</h2>
+
         <!-- DataTables Example -->
         <div class="card" style="margin-top: 80px;">
             <!-- <h3>Tanggal <?php echo $tanggal->tanggal?></h3>
@@ -11,16 +13,20 @@
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr class="text-center">
+                                <th>Tanggal</th>
                                 <th>Dokter Pemeriksa</th>
                                 <th>Nama Hewan</th>
+                                <th>Jenis</th>
                                 <th>Nama Pemilik</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php foreach ($detail as $d) : ?>
                                 <tr class="text-center">
+                                    <td><?php echo $d->tanggal?></td>
                                     <td><?php echo $d->namaPegawai?></td>
                                     <td><?php echo $d->namaHewan?></td>
+                                    <td><?php echo $d->jenis?></td>
                                     <td><?php echo $d->namaPemilik?></td>
                                 </tr>
                             <?php endforeach ?>
