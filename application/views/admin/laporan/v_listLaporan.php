5cb871b434122bd307066e9816b21b51de8017e4
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-21 14:51:04 +0700

    Update v_listLaporan.php

--- application/views/admin/laporan/v_listLaporan.php
+++ application/views/admin/laporan/v_listLaporan.php
@@ -59,7 +59,6 @@
 
       <div class="modal-footer">
         <button type="submit" class="btn btn-secondary" href="<?php echo base_url('Laporan/laporanTanggal') ?>">Unduh</button>
-        <!--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
       </div>
       <?php echo form_close(); ?>
 
