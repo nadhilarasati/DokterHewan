c4ff831b8e4507390207db822ed56ab21ea7f1b7
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-11-21 14:51:02 +0700

    Update v_detailLaporan.php

--- application/views/admin/laporan/v_detailLaporan.php
+++ application/views/admin/laporan/v_detailLaporan.php
@@ -2,12 +2,11 @@
 
     <div class="container-fluid">
 
-    <h2 class="text-center"style="margin-top:3cm;margin-bottom:1cm">Detail Laporan</h2>
+        <h2 class="text-center" style="margin-top:3cm;margin-bottom:1cm">Detail Laporan</h2>
 
         <!-- DataTables Example -->
         <div class="card" style="margin-top: 80px;">
-            <!-- <h3>Tanggal <?php echo $tanggal->tanggal?></h3>
-            <h3>Jenis Hewan <?php echo $jenis->jenis?></h3> -->
+
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -23,11 +22,11 @@
                         <tbody>
                             <?php foreach ($detail as $d) : ?>
                                 <tr class="text-center">
-                                    <td><?php echo $d->tanggal?></td>
-                                    <td><?php echo $d->namaPegawai?></td>
-                                    <td><?php echo $d->namaHewan?></td>
-                                    <td><?php echo $d->jenis?></td>
-                                    <td><?php echo $d->namaPemilik?></td>
+                                    <td><?php echo $d->tanggal ?></td>
+                                    <td><?php echo $d->namaPegawai ?></td>
+                                    <td><?php echo $d->namaHewan ?></td>
+                                    <td><?php echo $d->jenis ?></td>
+                                    <td><?php echo $d->namaPemilik ?></td>
                                 </tr>
                             <?php endforeach ?>
                         </tbody>
