530303acc1dfe6c21da5cc6c9eed64ec42c47322
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-12-04 13:40:09 +0700

    Update v_home.php

--- application/views/public/home/v_home.php
+++ application/views/public/home/v_home.php
@@ -31,36 +31,3 @@
 
     </div>
 </div>
-
-<div class="card-promo">
-    <div class="row">
-        <div class="col-4">
-            <div class="card card-1">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="card card-2">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="card card-3">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
