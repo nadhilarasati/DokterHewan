5b824ebd59283e45b3fa79f65e408dc582dd4803
Nadhila Larasati <nadhilarasati@Nadhila-Larasati.local>
2019-12-14 16:59:37 +0700

    Update v_aboutus.php

--- application/views/public/aboutus/v_aboutus.php
+++ application/views/public/aboutus/v_aboutus.php
@@ -1,15 +1,32 @@
 <div style="background-color: rgb(255, 249, 229); height:100%">
     <div style="padding-top:50px; padding-left:20px">
-        <h2 class="text-center">TENTANG KAMI</h2>
+        <h2 class="text-center">HUBUNGI KAMI</h2>
     </div>
-    <div class="row">
-        <div class="col-md-6" style="padding-top:70px; padding-left:50px">
-            <img src="../lib/img/nyanko.jpg">
-        </div>
-        <div class="col-md-6" style="padding-top:70px; padding-right:50px">
-        <p>Klinik Nyanko berdiri pada tahun 2016. Klinik yang didirikan oleh Drh. Oktavia Pusparini ini dulu merupakan sebuah pet shop. 
-            
-        </p>
+    <div class="card" style="margin-top:50px;margin-left:30px; margin-right:30px; margin-bottom: 30px; background-color: #ffcccc">
+
+        <div class="row">
+            <div class="col-md-6" style="padding-top:70px; padding-left:50px; margin-bottom:70px">
+                <img src="../lib/img/nyanko.jpg">
+            </div>
+            <div class="col-md-6" style="padding-top:125px; padding-right:50px">
+                <div>
+                    <i class="fa fa-home fa-2x" aria-hidden="true"></i>
+                    <span style="margin-left:20px">Jalan Wira Angun-Angun no. 16, Bandung</span>
+                </div>
+                <div style="margin-top:20px">
+                    <i class="fa fa-phone fa-2x" aria-hidden="true"></i>
+                    <span style="margin-left:20px">(022) 3528145</span>
+                </div>
+                <div style="margin-top:20px">
+                    <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                    <span style="margin-left:20px">nyanko.klinik@gmail.com</span>
+                </div>
+                <div style="margin-top:20px">
+                    <i class="fa fa-clock-o fa-2x" aria-hidden="true"></i>
+                    <span style="margin-left:20px">Senin - Sabtu, 08.00 - 17.00</span>
+                </div>
+                </p>
+            </div>
         </div>
     </div>
 </div>
